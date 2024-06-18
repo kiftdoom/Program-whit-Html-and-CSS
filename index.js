8388498d74087ef9e8f47ca477ce67192c858cfd
kiftdoom <159972365+kiftdoom@users.noreply.github.com>
2024-06-18 11:01:04 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function SubmitButtonClickom(){
+    let inputValue = document.getElementById("inpuText").value;
+    if(inputValue>0){
+            alert("the number is greath than 0")
+    }
+    if(inputValue<0){
+        alert("the number is less than 0")
+}
+if(inputValue==0){
+    alert("try agane")
+}
+}
